d4819320386be73aac59970bbc3e4b74b0771e78
Praveen_N <sunshinean94@gmail.com>
2026-04-27 00:51:45 +0530

    Time: 3 ms (100%), Space: 19.4 MB (100%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.py
+++ 3908-valid-digit-number/3908-valid-digit-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def validDigit(self, n: int, x: int) -> bool:
-        new_str = str(n)
-        new_x = str(x)
-        if new_x in new_str and new_x != new_str[0]:
+        number_str = str(n)
+        digit_x = str(x)
+        if digit_x in number_str and digit_x != number_str[0]:
             return True 
         else:
             return False
